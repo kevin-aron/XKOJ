a89c0a3af9b47b542af3b2549bc6dc76f6f55088
kevin-aron <1982153303@qq.com>
2023-04-14 18:35:05 +0800

    add view problem

---
+++ machine/views/content/viewproblem.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from machine.forms import UserForm
+from machine.models.coder.coder import Coder
+from machine.models.problem.problem import Problem
+
+def viewproblem(request,pid):
+	problem = get_object_or_404(Problem, code=pid)
+	return render(request, "oneproblem.html", {"problem":problem})
