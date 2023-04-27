f76ff7479f938c723f355f5e33ca16dfe5922851
kevin-aron <1982153303@qq.com>
2023-04-28 00:00:42 +0800

    finish game rk

--- machine/views/games/ingamerk.py
+++ machine/views/games/ingamerk.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from machine.models.coder.coder import Coder
+from machine.models.problem.problem import Problem
+from machine.models.game.game import Game
+from machine.models.game.gameproblem import GameProblem
+from machine.models.game.gamesubmission import GameSubmission
+from machine.models.game.gamerk import GameRk
 
 def ingamerk(request,pid):
-	return render(request, 'games/ingamerk.html')
+	game = get_object_or_404(Game,code=pid)
+	gamerk = GameRk.objects.filter(game=game).order_by('-totalac', 'failtime')
+	problems = GameProblem.objects.filter(game=game).order_by('addnum')
+	submissions = GameSubmission.objects.filter(game=game)
+	return render(request, 'games/ingamerk.html', {"gamerk":gamerk, "problems":problems, "submissions":submissions,"gid":pid})
--- machine/views/games/ingamestatement.py
+++ machine/views/games/ingamestatement.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from machine.models.coder.coder import Coder
+from machine.models.problem.problem import Problem
+from machine.models.game.game import Game
+from machine.models.game.gamesubmission import GameSubmission
 
 def ingamestatement(request,pid):
 	return render(request, 'games/ingamestatement.html')
